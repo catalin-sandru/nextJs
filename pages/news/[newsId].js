7a9b0e8a4468eddaaf4485845d061af879514836
Catalin <catalin.sandru88@gmail.com>
2021-11-20 17:52:24 +0200

    get route details from dynamic page with useRouter

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,7 +1,15 @@
 // this file has a dynamic routing with an identifier in brackets as a filename
 // this file will be rendered at domain.com/news/anything-else
 
+import { useRouter } from "next/router";
+
 const DetailPage = () => {
+  // holds details about the path
+  const router = useRouter();
+
+  // holds details for the path that is inserted after news/
+  router.query.newsId;
+
   return <h1>The detail page</h1>;
 };
 
