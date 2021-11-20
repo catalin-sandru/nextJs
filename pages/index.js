4faff2c78c85b64ebaefcfb91da5252fee6e1cdc
Catalin <catalin.sandru88@gmail.com>
2021-11-20 18:01:16 +0200

    navigating in next with anchor tag

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 // this page will be rendered at the / or home page
 
+import { Fragment } from "react";
+
 const HomePage = () => {
-  return <h1>the home page</h1>;
+  return (
+    <Fragment>
+      <h1>the home page</h1>;
+      <ul>
+        <li>
+          {/* using an anchor tag the page will send a request to the server and the page will reload */}
+          <a href="/news/nextJs-is-great">NextJS is great</a>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default HomePage;
