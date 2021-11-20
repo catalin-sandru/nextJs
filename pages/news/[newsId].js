1a180e96350df58c5d83ae29b65e8156bb41fa6b
Catalin <catalin.sandru88@gmail.com>
2021-11-20 17:45:33 +0200

    added dynamic routes with special filename

---
+++ pages/news/[newsId].js
@@ -0,0 +1,8 @@
+// this file has a dynamic routing with an identifier in brackets as a filename
+// this file will be rendered at domain.com/news/anything-else
+
+const DetailPage = () => {
+  return <h1>The detail page</h1>;
+};
+
+export default DetailPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,4 @@
-// nextjs will render this file at domain.com/filename so no routing is required
+// nextjs will render this file at domain.com/folder-name so no routing is required
 
 const NewsPage = () => {
   return <h1>the News page</h1>;
