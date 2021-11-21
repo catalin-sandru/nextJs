aa57b491eaa3cda1e20d937532ebe892515c0899
Catalin <catalin.sandru88@gmail.com>
2021-11-21 22:18:26 +0200

    added Layout to _app.js to render the same layout across the website

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // we wrap Component with Layout to have the same styles across the pages
+    // if we didn't do this, we have to import and include the layout in each component that we want such a layout
+    <Layout>
+      {/* Component is the one that gets rendered and changes at every page */}
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
