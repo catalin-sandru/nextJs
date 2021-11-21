06a6479c1c7dc82f363fb217b2a508bb1286ba1a
Catalin <catalin.sandru88@gmail.com>
2021-11-21 22:25:03 +0200

    redirect programatically with useRouter hook

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,7 +1,17 @@
-import Card from '../ui/Card';
-import classes from './MeetupItem.module.css';
+import { useRouter } from "next/router";
+
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
+  const router = useRouter();
+
+  const showDetailsHandler = () => {
+    // push is a method in router that changes the url programatically.
+    // In case we don't want to use a anchor tag to navigate between app pages
+    router.push("/" + props.id);
+  };
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +23,7 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
