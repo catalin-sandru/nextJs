8147c6b3e3770185ea20ffeb5dcbb4330bd1f193
Catalin <catalin.sandru88@gmail.com>
2021-11-22 13:46:41 +0200

    getStaticPaths.. see commit

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -15,4 +15,41 @@ const MeetupDetails = () => {
   );
 };
 
+// this function in required to tell nextJs for whitch dynamic params this page should be pre-generated
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const params = context.params;
+  console.log(context);
+
+  return {
+    props: {
+      meetupData: {
+        id: "m1",
+        image:
+          "https://about.gitlab.com/images/blogimages/get-involved-with-gitlab-meetups/japanmeetup.jpg",
+        title: "A First Meetup",
+        address: "Some Address",
+        description: "The meetup description.",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
