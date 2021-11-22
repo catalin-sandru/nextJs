bcfd07bb797558e28e8fc9fcd6fd6a5a6ffc9cca
Catalin <catalin.sandru88@gmail.com>
2021-11-22 18:54:23 +0200

    mongoDb setup

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,24 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://Admin:Admin1234@cluster0.sorwh.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupCollection = db.collection("meetups");
+
+    const result = await meetupCollection.insertOne(data);
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup Added" });
+  }
+}
+
+export default handler;
