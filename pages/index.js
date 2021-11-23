30ff411ce5dcb1338a7346bd75c3eb7952301818
Catalin <catalin.sandru88@gmail.com>
2021-11-23 23:58:06 +0200

    fetching data from DB directly from getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { MongoClient } from "mongodb";
+
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -26,10 +28,26 @@ const HomePage = (props) => {
 // this function needs to be named getStaticProps
 // on render, the component waits for this function to execute, passes props, and AFTER renders the component
 export async function getStaticProps() {
-  // in here we can fetch api data
+  // as this code runs on the server we can fetch data from mongoDb without an Api call
+  const client = await MongoClient.connect(
+    "mongodb+srv://Admin:Admin1234@cluster0.sorwh.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((m) => ({
+        title: m.title,
+        address: m.address,
+        image: m.image,
+        id: m._id.toString(),
+      })),
     },
     // revalidate will reEvaluate the component on the server in case new data is added
     // the value is number in seconds on how often the component should re-evaluate in seconds
